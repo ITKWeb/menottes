99074cba66199c9d526195665a460ce9229808f7
bastien <bastien.amar@itkweb.com>
2013-07-09 10:22:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Menottes::Application.config.secret_key_base = '530e7e5123d2f2956af91006d123b69f409dd4cf4c2ec73da64b1245f98161986e40b6258f36270c55012f6d354bd667f9bdca202c5a8901a1d04f163c28dfee'
