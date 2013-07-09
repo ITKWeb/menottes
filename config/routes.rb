46d2fd7415625da0cfbe1b0b21837c71d106084f
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-07-09 10:46:01 +0200

    initial client commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Menottes::Application.routes.draw do
+
+  root :to => "static#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
