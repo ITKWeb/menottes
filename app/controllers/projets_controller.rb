2b862903f8011e80465e422fe75b4abb2eef99b7
bastien <bastien.amar@itkweb.com>
2013-07-09 15:59:38 +0200

    Create de projet, avec lecture des infos dans la requete POST

--- app/controllers/projets_controller.rb
+++ app/controllers/projets_controller.rb
@@ -1,6 +1,23 @@
 class ProjetsController < ApplicationController
+
+	#La ligne en dessous nous permet d'avoir un trou de sécurité (cf CRSF, man in the middle)
+	skip_before_filter :verify_authenticity_token
+	
 	def index
 		@projets = Projet.all
 		render json: @projets
+	end
+
+	def create
+		projet = request.raw_post
+		p = JSON.parse(projet, :object_class => Projet)
+		p.save
+		render json: p
+	end
+
+	def destroy
+		projet = Projet.find(params[:idProjet])
+		projet.destroy
+		render json: true
 	end
 end
