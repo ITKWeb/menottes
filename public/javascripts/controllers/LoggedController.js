4adb6f379f9db94f2ef37744e144d193100ae7cf
laurentd <laurent.dufour33@gmail.com>
2013-07-10 21:35:17 +0200

    CSS update

--- public/javascripts/controllers/LoggedController.js
+++ public/javascripts/controllers/LoggedController.js
@@ -2,10 +2,12 @@ app.controller('LoggedController', ['$scope', 'Network', '$routeParams', '$rootS
     
     $network.getSprints(function(sprints) {
         $scope.sprints = sprints;
+		//$scope.selected = $scope.sprints[0]; //Pour le premier sélectionné par défaut -> a voir car aucun vraiment sélectionné par défaut. Il faudrait que le Backlog le soit.
     }, $routeParams.projectId);
     
     $scope.clickOnSprint = function(sprint) {
 	console.log(sprint);
+		$scope.selected = sprint;
         $network.getTickets(function(tickets) {
             $scope.tickets = tickets;
         }, $routeParams.projectId, sprint.id);
