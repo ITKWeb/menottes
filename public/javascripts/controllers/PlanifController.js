e9b2e9a1900e7c4115cd98473b0872ddfd5d84dc
laurentd <laurent.dufour33@gmail.com>
2013-08-30 15:16:18 +0200

    Plannif again

--- public/javascripts/controllers/PlanifController.js
+++ public/javascripts/controllers/PlanifController.js
@@ -1,26 +1,6 @@
-app.controller('PlanifController', ['$scope', 'Network', '$routeParams', '$rootScope', function($scope, $network, $routeParams, $rootScope) {
+app.controller('PlanifController', ['$scope', 'Network', function($scope, $network) {
     
-    $network.getSprints(function(sprints) {
-        $scope.sprints = sprints;
-		//Ajout By Laurent
-			//pour que le Backlog soit selected direct et que ses tickets apparaissent cash aussi
-			//A vérifier donc...
-			$scope.selected = $scope.sprints[0];
-			$network.getTickets(function(tickets) {
-				$scope.tickets = tickets;
-			}, $routeParams.projectId, sprints[0].id);
-		//fin d'ajout by Laurent
-	}, $routeParams.projectId);
-    
-    $scope.clickOnSprint = function(sprint) {
-	console.log(sprint);
-		$scope.selected = sprint;
-        $network.getTickets(function(tickets) {
-            $scope.tickets = tickets;
-        }, $routeParams.projectId, sprint.id);
-    };
-    
-    $rootScope.$on('moveColumnEvent', function(evt, dragged, dropped) {
-        console.log(line1, line2);
+    $network.getProjets(function(projets) {
+        $scope.projets = projets;
     });
 }]);
