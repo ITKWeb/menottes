39e7c370416ceb69bc8eb1e0e10c80a973150f63
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-08-30 13:35:01 +0200

    fix ror vs angularjs post

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('app', []);
 
-app.config(["$routeProvider",
-  function($routeProvider) {
+app.config(["$routeProvider", "$httpProvider",
+  function($routeProvider, $httpProvider) {
+    $httpProvider.defaults.headers['common']['X-Requested-With'] = 'XMLHttpRequest';
     $routeProvider
       .when('/', {
         templateUrl: 'views/login.html',
