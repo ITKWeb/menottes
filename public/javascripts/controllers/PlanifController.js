782c5bca7468ffd8c6e855f68da78c75dd72fff4
laurentd <laurent.dufour33@gmail.com>
2013-08-30 15:25:42 +0200

    again again

--- public/javascripts/controllers/PlanifController.js
+++ public/javascripts/controllers/PlanifController.js
@@ -1,6 +1,5 @@
 app.controller('PlanifController', ['$scope', 'Network', function($scope, $network) {
     
-
     $network.getProjets(function(projets) {
         $scope.projets = projets;
     });
