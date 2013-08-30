95f1e7e38ddaee442778ddac652d3464f7c29fa7
Marc <marc@marc-Latitude-E6520.(none)>
2013-08-30 16:08:51 +0200

    récupération des paramètres depuis l'url

--- public/javascripts/controllers/ChoiceController.js
+++ public/javascripts/controllers/ChoiceController.js
@@ -1,9 +1,13 @@
 app.controller('ChoiceController', ['$scope', '$location' ,function($scope, $location) {
   var url = $location.path().split("/");
+  $scope.title="";
     if (url.length === 3){
-    console.log("initialisation du sondage "+url[2]);
+      var pollid=+url[2];
+    console.log("initialisation du sondage "+pollid);
+    $scope.title="Sondage "+pollid;
   }else{
       console.log("Nouveau sondage");
+      $scope.title="Nouveau sondage";
   }
 }
 ]);
