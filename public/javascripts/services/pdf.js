9c7ed525479b7a44641e4c77a5e10b5919162dcf
Romain MANESCHI <rmaneschi@ITK-P-201202-03.(none)>
2013-10-08 16:03:11 +0200

    pdf when x tickets

--- public/javascripts/services/pdf.js
+++ public/javascripts/services/pdf.js
@@ -13,6 +13,7 @@ app.factory("Pdf", [function () {
 			console.log(tickets);
 
 			var doc = new jsPDF();
+			var nb_demi_page = 0;
 
 			console.log(doc);
 
@@ -20,20 +21,25 @@ app.factory("Pdf", [function () {
 
 				var ticket = tickets[i];
 
-				doc.text(PADDING, 20 + (middle_page * i), ticket.titre);
-				doc.text(MAX_X-30-PADDING, 20 + (middle_page * i), "Importance");
-				doc.rect(MAX_X-PADDING, 13 + (middle_page * i), 20, 10);
-				doc.text(PADDING, 30 + (middle_page * i), ticket.description);
-				doc.text(MAX_X-30-PADDING, 30 + (middle_page * i), "Estimation");
-				doc.rect(MAX_X-PADDING, 23 + (middle_page * i), 20, 10);
-				doc.text(PADDING, 40 + (middle_page * i), "Notes");
-				doc.rect(PADDING, 41 + (middle_page * i), 120, 25);
-				doc.text(PADDING, 75 + (middle_page * i), "Comment démontrer");
-				doc.rect(PADDING, 76 + (middle_page * i), 120, 25);
+				doc.text(PADDING, 20 + (middle_page * nb_demi_page), ticket.titre);
+				doc.text(MAX_X-30-PADDING, 20 + (middle_page * nb_demi_page), "Importance");
+				doc.rect(MAX_X-PADDING, 13 + (middle_page * nb_demi_page), 20, 10);
+				doc.text(PADDING, 30 + (middle_page * nb_demi_page), ticket.description);
+				doc.text(MAX_X-30-PADDING, 30 + (middle_page * nb_demi_page), "Estimation");
+				doc.rect(MAX_X-PADDING, 23 + (middle_page * nb_demi_page), 20, 10);
+				doc.text(PADDING, 40 + (middle_page * nb_demi_page), "Notes");
+				doc.rect(PADDING, 41 + (middle_page * nb_demi_page), 120, 25);
+				doc.text(PADDING, 75 + (middle_page * nb_demi_page), "Comment démontrer");
+				doc.rect(PADDING, 76 + (middle_page * nb_demi_page), 120, 25);
 
-				doc.line(PADDING, MAX_Y/2, MAX_X, MAX_Y/2);
+				if(nb_demi_page === 0) {
+					doc.line(PADDING, MAX_Y/2, MAX_X, MAX_Y/2);
+				}
 
-				if(i !== 0 && i%2 === 0) {
+				nb_demi_page++;
+
+				if(i%2 !== 0) {
+					nb_demi_page = 0;
 					doc.addPage();
 				}
 
